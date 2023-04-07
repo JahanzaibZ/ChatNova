95b86fae00a7ec75a355ed3c142f71fb885bd196
Jahanzaib <work.officialjz@gmail.com>
2023-04-07 16:52:47 +0500

    Worked on data model classes

---
+++ lib/models/chat.dart
@@ -0,0 +1,11 @@
+class Chat {
+  final String? id;
+  final String? receiverId;
+  final DateTime? lastMessageTimeStamp;
+
+  Chat({
+    this.id,
+    this.receiverId,
+    this.lastMessageTimeStamp,
+  });
+}
---
+++ lib/models/message.dart
@@ -0,0 +1,15 @@
+class Message {
+  final String? id;
+  final String? text;
+  final DateTime? timeStamp;
+  final String? recieveStatus;
+  final String? chatId;
+
+  Message({
+    this.id,
+    this.text,
+    this.timeStamp,
+    this.recieveStatus,
+    this.chatId,
+  });
+}
